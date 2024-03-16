c07f38599f2ce9e9caac2f4932c6441a0d7dc181
Vipin Saini <vipinsaini212704@gmail.com>
2024-03-16 14:45:02 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { userRouter } from './routes/user.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 export const app = express();
 
@@ -8,3 +9,8 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use('/api/v1', userRouter);
+app.use(express.static(path.resolve('./frontend/build')));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve('./frontend/build/index.html'));
+});
