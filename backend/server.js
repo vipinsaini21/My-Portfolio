e469d86ce93b56b74e8a388d569f4e5bba825d0d
Vipin Saini <vipinsaini212704@gmail.com>
2024-03-17 12:37:31 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { connectDatabase } from './config/database.js';
 import cloudinary from 'cloudinary';
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: './backend/config/config.env' });
 
 connectDatabase();
 
